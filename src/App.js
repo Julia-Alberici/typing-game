4aa1571be5b46fb9229663b4bee6c2c820c22226
Julia Alberici <julia.jpa0@gmail.com>
2022-09-13 19:43:49 -0300

    add text and time state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+
+  function wordCounter(text) {
+    const words = text.trim().split(' ');
+    return words.filter(word => word !== '').length;
+  }
+
+  useEffect(() =>{
+    if(timeRemaining > 0){
+      setTimeout(() => {
+        setTimeRemaining( time => time -1);
+      }, 1000)
+    }
+  }, [timeRemaining])
+
   return (
     <>
       <h1>How fast do you type?</h1>
-      <textarea />
-      <h4>Time reminaing: ???</h4>
-      <button>Start</button>
+      <textarea value={text} onChange ={e => setText(e.target.value)} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => wordCounter(text)}>Start</button>
       <h1>Word count: ???</h1>
     </>
   );
