e8a9e5fc28e238a9325eca7db51b063edf1a80a1
Julia Alberici <julia.jpa0@gmail.com>
2022-09-12 22:54:26 -0300

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>Time reminaing: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
+    </>
   );
 }
 
