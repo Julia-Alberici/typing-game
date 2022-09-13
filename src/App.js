c0bb56c30bc0ed1caeae215b02b51afbe20af680
Julia Alberici <julia.jpa0@gmail.com>
2022-09-13 19:57:01 -0300

    add start state and improve useEffect timer

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import {useEffect, useState} from 'react';
 function App() {
   const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState(5);
+  const [hasGameStarted, setHasGameStarted] = useState(false);
 
   function wordCounter(text) {
     const words = text.trim().split(' ');
     return words.filter(word => word !== '').length;
   }
 
-  useEffect(() =>{
-    if(timeRemaining > 0){
+  useEffect(() => {
+    if(timeRemaining > 0 && hasGameStarted) {
       setTimeout(() => {
         setTimeRemaining( time => time -1);
       }, 1000)
+    } else{
+      setHasGameStarted(false);
     }
-  }, [timeRemaining])
+  }, [timeRemaining, hasGameStarted])
 
   return (
     <>
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange ={e => setText(e.target.value)} />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={() => wordCounter(text)}>Start</button>
+      <button onClick={() => setHasGameStarted(true)}>Start</button>
       <h1>Word count: ???</h1>
     </>
   );
